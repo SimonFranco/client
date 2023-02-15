fac5307db968add509b8c2d6f46179062fa0cef4
Simon Franco <simonfranco0@gmail.com>
2023-02-14 16:46:54 -0800

    navbar complete

--- src/scenes/homePage/index.jsx
+++ src/scenes/homePage/index.jsx
@@ -1,7 +1,15 @@
+import { Box } from "@mui/system"
+import Navbar from "../navbar/index"
+
 const HomePage = ( )=>{
-    return <div>homepage</div>
+    return (
+        <Box>
+        <Navbar />
+        </Box>
 
 
+
+    )
 }
 
 export default HomePage
